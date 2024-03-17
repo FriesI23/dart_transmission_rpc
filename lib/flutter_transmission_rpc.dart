e4d167df76556e2fe1129a1099ccac2e695daf8a
Fries_I23 <FriesI23@outlook.com>
2024-03-17 18:33:07 +0800

    chore: move test about files to root demo folder

--- lib/flutter_transmission_rpc.dart
+++ lib/flutter_transmission_rpc.dart
@@ -1,1 +1,7 @@
 library flutter_transmission_rpc;
+
+export "./src/client.dart"
+    show
+        TransmissionRpcClient,
+        TransmissionRpcRetryReason,
+        TransmissionRpcClientFrameExtension;
