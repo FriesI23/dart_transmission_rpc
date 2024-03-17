a3ab2ca01fe76862fcbb7c19b9952fff5238812a
Fries_I23 <FriesI23@outlook.com>
2024-03-17 20:12:00 +0800

    chore: add build_runner script

---
+++ scripts/build_runner.bat
@@ -0,0 +1,7 @@
+rem Copyright (c) 2024 Fries_I23
+rem
+rem This software is released under the MIT License.
+rem https://opensource.org/licenses/MIT
+
+cd /d "%~dp0\.."
+dart run build_runner build --delete-conflicting-outputs
